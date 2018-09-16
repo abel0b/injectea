ed76464216d6a24d8b5f0337e38fe48009a39117
Abel <abelcd@protonmail.com>
2018-09-16 16:19:55 +0000

    fix: scoped service

--- test/hierarchical-container.test.js
+++ test/hierarchical-container.test.js
@@ -7,11 +7,19 @@ export const lab = Lab.script()
 lab.test('create child containers', async () => {
     const container = new Container()
 
-    const child = new Container(container)
-
     container.child('test')
 
     expect(container.scope('test')).to.be.instanceof(Container)
+})
+
+lab.test('try to access to undefined scope', async () => {
+    const container = new Container()
+
+    container.value('foo', 42)
+
+    expect(() => {
+        container.scope('foo')
+    }).to.throw(Error)
 })
 
 lab.test('resolve dependency from child container', async () => {
@@ -28,10 +36,11 @@ lab.test('resolve dependency from child container', async () => {
     container.child('test')
 
     container.register(Foo)
-    container.scope('test').register(FakeFoo)
+    container.register(FakeFoo)
 
     expect(() => {container.scope('production')}).to.throws(Error)
 
     expect(await container.resolve('Foo')).to.be.instanceof(Foo)
+    expect(container.resolve('Undefined')).to.reject(Error)
     expect(await container.scope('test').resolve('Foo')).to.be.instanceof(FakeFoo)
 })
